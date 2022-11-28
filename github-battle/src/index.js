64065274bf1c1f71130bb23fa7137f1de67edaa4
ravindrapv <ravindra.ravi4849@gmail.com>
2022-11-28 23:49:25 +0530

    github-battle

---
+++ github-battle/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./Component/App";
+import "./styles/index.css";
+import "./styles/style.css";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
